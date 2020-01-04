a3804172b0396e062159d23a59d9fb4abcd034da
gdpark <gd.park5242@gmail.com>
2020-01-04 19:45:58 +0900

    Fix Dependency error

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,45 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
+import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper'
-import { response } from 'express';
 
 const styles = theme => ({
-  root:{
-    width : '100%',
+  root: {
+    width: "100%",
     marginTop: theme.spacing.unit * 3,
     overflowX: "auto"
   },
   table: {
     minWidth: 1080
   }
-})
-
+});
 
 class App extends Component {
 
   state = {
-    customers:""
+    customers: ''
   }
 
-  // 컴포넌트 마운트시 실행
-  componentDidMount(){
-  this.callApi()
-  .then(res => this.setState({customers: res}))
-  .catch(err => console.log(err));
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
   }
 
-  callApi = async() => {
+  callApi = async () => {
     const response = await fetch('/api/customers');
-      const body = await response.json();
-      return body;
+    const body = await response.json();
+    return body;
   }
 
-  render(){
-
-    const {classes} = this.props;
-
+  render() {
+    const { classes } = this.props;
     return (
       <Paper className={classes.root}>
         <Table className={classes.table}>
@@ -59,44 +54,14 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-          {
-          this.state.customers ? 
-          this.state.customers.map(c => {
-              return <Customer
-              key={c.id}
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-              />
-            }) : ""
-          }
-
+            {this.state.customers ? this.state.customers.map(c => {
+              return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+            }) : ''}
           </TableBody>
-
-
         </Table>
-    </Paper>
-      );
+      </Paper>
+    );
   }
 }
 
 export default withStyles(styles)(App);
-
-/*
- {
-        customers.map(c => {
-            return <Customer
-            key={c.id}
-            id={c.id}
-            image={c.image}
-            name={c.name}
-            birthday={c.birthday}
-            gender={c.gender}
-            job={c.job}
-            />
-          })
-        }
-*/ 
--- server.js
+++ server.js
@@ -1,40 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app =express();
+const app = express();
 const port = process.env.PORT || 5000;
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/customers', (req, res) => {
     res.send([
         {
-        'id':1,
-        'image':'http://placeimg.com/64/64/1',
-        'name': '가길동',
-        'birthday': '2019-11-11',
-        'gender': '남자',
-        'job': '대학생1'
+            'id': 1,
+            'image': 'https://placeimg.com/64/64/1',
+            'name': '홍길동',
+            'birthday': '961222',
+            'gender': '남자',
+            'job': '대학생'
         },
         {
-          'id':2,
-          'image':'http://placeimg.com/64/64/2',
-          'name': '나길동',
-          'birthday': '2019-11-10',
-          'gender': '여자',
-          'job': '대학생2'
-          },
-          {
-            'id':3,
-            'image':'http://placeimg.com/64/64/3',
-            'name': '다길동',
-            'birthday': '2019-11-9',
+            'id': 2,
+            'image': 'https://placeimg.com/64/64/2',
+            'name': '나동빈',
+            'birthday': '960508',
             'gender': '남자',
-            'job': '대학생3'
-            }
-      ]
-    )
-})
+            'job': '프로그래머'
+        },
+        {
+            'id': 3,
+            'image': 'https://placeimg.com/64/64/3',
+            'name': '이순신',
+            'birthday': '961127',
+            'gender': '남자',
+            'job': '디자이너'
+        }
+    ]);
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
